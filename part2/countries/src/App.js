adc3ec549d95250c7fd85da5c4e877d4036d18e5
TylerBlood <tylerlblood@gmail.com>
2020-05-11 17:11:21 -0400

    fixed unlimited api calls, refactored

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -8,18 +8,18 @@ const App = () => {
   const [newSearch, setSearch] = useState('')
   
   useEffect(() => {
-    axios.get('https://restcountries.eu/rest/v2/all')
-      .then(response => {
-        setCountries(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  })
+    if (countries.length < 1){
+      axios.get('https://restcountries.eu/rest/v2/all')
+        .then(response => {
+          setCountries(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+  }})
 
   const handleSearch = (event) => {
     setSearch(event.target.value);
-    const searchMatch = countries.filter(country => country.name.includes(newSearch));
   }
 
   return (
--- part2/countries/src/Components/List.js
+++ part2/countries/src/Components/List.js
@@ -4,8 +4,6 @@ const List = ({ countries, newSearch }) => {
   const regex = new RegExp(newSearch, 'gi');
   const nameMatches = countries.filter(country => country.name.match(regex));
   const matches = [...nameMatches];
-  console.log(matches);
-  console.log(matches.languages);
   if (matches.length > 10){
     return(
       <p>Too many matches, specify another filter</p>
@@ -13,29 +11,24 @@ const List = ({ countries, newSearch }) => {
   } else if (matches.length === 1){
     return(
       <>
-      {matches.map((match, i) => 
-      (
-        <>
-          <h1>{match.name}</h1>
-          <p>capital: {match.capital}</p>
-          <p>population: {match.population}</p>
-          <h2>Languages:</h2>
-          <ul>
-            {match.languages.map(language =>
-              <li>{language.name}</li>
-            )}
-          </ul>
-          <img src={match.flag} alt="flag" height="250px" />
-        </>
-      ))}
+        <h1>{matches[0].name}</h1>
+        <p>capital: {matches[0].capital}</p>
+        <p>population: {matches[0].population}</p>
+        <h2>Languages:</h2>
+        <ul>
+          {matches[0].languages.map(language =>
+            <li key={language.name}>{language.name}</li>
+          )}
+        </ul>
+        <img src={matches[0].flag} alt="flag" height="250px" />
       </>
     )
-  } else{
+  } else {
     return (
       <ul>
           {matches.map((match) =>
           (
-            <li>{match.name}</li>
+            <li key={match.alpha3Code}>{match.name}</li>
         ))}
       </ul>
     )
